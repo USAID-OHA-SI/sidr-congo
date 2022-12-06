9f2a8d3ddb0978aa79534b75f620d65a09f9af58
jhoehner <jhoehner@usaid.gov>
2022-12-06 16:02:16 -0500

    checking work

--- Scripts/FY22_Q4_ClinicalCascades.R
+++ Scripts/FY22_Q4_ClinicalCascades.R
@@ -4,7 +4,7 @@
 # REF ID:   fab3899d
 # LICENSE:  MIT
 # DATE:     2022-11-10
-# UPDATED:  2022-11-10
+# UPDATED:  2022-12-01
 
 # DEPENDENCIES -----------------------------------------------------------------
 
@@ -31,13 +31,15 @@ ref_id <- "fab3899d"
 
 # metadata
 path <- si_path() %>%
-    return_latest("Genie_PSNU_IM_Democratic_Republic_of_the_Congo")
+    return_latest("MER_Structured_Datasets_OU_IM_FY20-23_20221114_v1_1")
+
+ou_df <- read_msd(path)
 
 get_metadata(path)
 
 # PSNU
 psnu_df <- si_path() %>%
-    return_latest("Genie_PSNU_IM_Democratic_Republic_of_the_Congo") %>%
+    return_latest("Genie_PSNU_IM_Democratic_Republic_of_the_Congo_Frozen_149c4e61-c0d2-4668-b44a-6a593e40a538") %>%
     read_msd()
 
 genie_path <- path
@@ -46,20 +48,41 @@ curr_pd <-metadata$curr_pd
 data_source <- metadata$source
 
 # IMPORT -----------------------------------------------------------------------
+drc_df <- ou_df %>%
+    filter(
+        country == "Democratic Republic of the Congo",
+        fiscal_year == "2022")
 
-df_msd <- psnu_df %>%
-  filter(
-    funding_agency == "USAID",
-    fiscal_year == "2022")
+drc_usaid_df <- ou_df %>%
+    filter(
+        country == "Democratic Republic of the Congo",
+        funding_agency == "USAID",
+        fiscal_year == "2022")
+
+drc_psnu_df <- psnu_df %>%
+    filter(fiscal_year == "2022")
+
+drc_usaid_psnu_df <- psnu_df %>%
+    filter(
+        funding_agency == "USAID",
+        fiscal_year == "2022")
 
 # MUNGE ------------------------------------------------------------------------
 
-# return standard cascade (1) data as an example
-std_casc_df <- return_cascade(df_msd, 1)
+# Generate all cascade plots for country-wide data
+batch_cascade_plot(drc_df,
+  imgpath = "Images/cascade/all", imgtype = ".png")
 
-# Generate all cascade plots for country-wide data, most recent Q
-batch_cascade_plot(df_msd,
-  imgpath = "Images/cascade/usaid", imgtype = ".png")
+# Generate all cascade plots for usaid specific data
+batch_cascade_plot(drc_usaid_df,
+                   imgpath = "Images/cascade/usaid", imgtype = ".png")
+
+# # export plots individually if sizing looks funky
+# return_cascade_plot(drc_usaid_df)
+# #
+# si_save(paste0("kp_cascade.png"),
+#          path = "Images/cascade/usaid",
+#          scale = 1.6)
 
 # Return Standard Cascade Plots for each SNU,
 # Note: these are all the plots you can choose from
@@ -78,19 +101,48 @@ batch_cascade_plot(df_msd,
 # 12:Adults Male
 # 13:KP
 
+# All agencies
+
 # Haut Katanga
-batch_cascade_plot(df_msd %>%
+batch_cascade_plot(drc_psnu_df %>%
                        filter(snu1 == "Haut Katanga"),
-                   imgpath = "Images/cascade/Haut_katanga/usaid", imgtype = ".png")
+                   imgpath = "Images/cascade/Haut_Katanga", imgtype = ".png")
+
+# export plots individually if sizing looks funky
+return_cascade_plot(drc_psnu_df %>%
+                        filter(snu1 == "Haut Katanga"))
+
+si_save(paste0("hk_std_cascade.png"),
+        path = "Images/cascade/Haut_Katanga",
+        scale = 1.6)
 
 # Kinshasa
-batch_cascade_plot(df_msd %>%
+batch_cascade_plot(drc_psnu_df %>%
+                       filter(snu1 == "Kinshasa"),
+                   imgpath = "Images/cascade/Kinshasa", imgtype = ".png")
+
+
+# Lualaba
+batch_cascade_plot(drc_psnu_df %>%
+                       filter(snu1 == "Lualaba"),
+                   imgpath = "Images/cascade/Lualaba", imgtype = ".png")
+
+
+# USAID specific
+
+# Haut Katanga
+batch_cascade_plot(drc_usaid_psnu_df  %>%
+                       filter(snu1 == "Haut Katanga"),
+                   imgpath = "Images/cascade/Haut_Katanga/usaid", imgtype = ".png")
+
+# Kinshasa
+batch_cascade_plot(drc_usaid_psnu_df  %>%
                        filter(snu1 == "Kinshasa"),
                    imgpath = "Images/cascade/Kinshasa/usaid", imgtype = ".png")
 
 
 # Lualaba
-batch_cascade_plot(df_msd %>%
+batch_cascade_plot(drc_usaid_psnu_df  %>%
                        filter(snu1 == "Lualaba"),
                    imgpath = "Images/cascade/Lualaba/usaid", imgtype = ".png")
 
